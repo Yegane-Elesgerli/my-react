410b5cc18ed87519f8e0b1c90162b591b418ea9c
yegane alasgarli <63316166+Yegane-Elesgerli@users.noreply.github.com>
2022-12-17 12:55:54 +0400

    nm

---
+++ src/Component/Personal/Personal.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import React, { Component } from 'react'
+
+ class Personal extends Component {
+  render() {
+    return (
+      <div>ad:Ali;
+        yash:25
+        yeni
+        ytr
+      </div>
+    )
+  }
+}
+
+export default  Personal
